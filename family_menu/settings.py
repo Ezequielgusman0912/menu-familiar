65a84c6611550d0983e8c38b5326f780a15bac87
Ezequielgusman0912 <egusman@portahnos.com.ar>
2026-06-09 14:57:54 -0300

    push

--- family_menu/settings.py
+++ family_menu/settings.py
@@ -75,10 +75,10 @@ WSGI_APPLICATION = 'family_menu.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
+    "default": dj_database_url.config(
+        default=os.environ.get("DATABASE_URL"),
         conn_max_age=600,
-        conn_health_checks=True,
+        ssl_require=True,
     )
 }
 
